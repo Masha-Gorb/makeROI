89a218a97a80cf3bd959008afebcae33e98db3ec
Masha Gorbunova <masha.gorbunova@gmail.com>
2023-01-13 00:01:05 +0300

    add -> modal window and other stuff

--- index.js
+++ index.js
@@ -23,3 +23,38 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
+
+
+function validateInput(input_id, output_id, pattern_text, valid_message, invalid_message) {
+	var form = document.getElementById("form");
+	var data = document.getElementById(input_id).value;
+	var result = document.getElementById(output_id);
+	if (data.match(pattern_text)) {
+		form.classList.add("valid");
+		form.classList.remove("invalid");
+		result.innerHTML = valid_message;
+		result.style.color = "#00ff00";
+	} else {
+		form.classList.remove("valid");
+		form.classList.add("invalid");
+		result.innerHTML = invalid_message;
+		result.style.color = "#ff0000";
+	}
+	if (data === "") {
+		form.classList.remove("valid");
+		form.classList.remove("invalid");
+		result.innerHTML = "";
+		result.style.color = "#00ff00";
+	}
+
+}
+
+function validationName() {
+	var pattern = "[a-zA-Z]{10}*";
+	validateInput("name", "text2", pattern, "Имя подходит.", "Пожалуйста введите правильное имя");
+}
+
+function validationTel() {
+	var pattern = "\\+[7-8]{10}";
+	validateInput("tel", "text1", pattern, "Телефон подходит.", "Пожалуйста введите правильный телефон.");
+}
